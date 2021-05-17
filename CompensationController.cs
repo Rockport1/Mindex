9659439af1d80d0753d681e92753afc773e5c015
Rockport1 <petersoner@hotmail.com>
2021-05-17 07:53:55 -0400

    Add files via upload

---
+++ CompensationController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using challenge.Services;
+using challenge.Models;
+using challenge.resources;
+using AutoMapper;
+using Microsoft.Extensions.Logging;
+using challenge.Extensions;
+
+namespace challenge.Controllers
+{
+    [Route("api/compensation")]
+    public class CompensationController : Controller
+    {
+        private readonly ILogger _logger;
+        private readonly ICompensationService _compensationService;
+        private readonly IMapper _mapper;
+
+        public CompensationController(ILogger<ReportingStructureController> logger, ICompensationService compensationService, IMapper mapper)
+        {
+            _logger = logger;
+            _compensationService = compensationService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        //public IActionResult CreateCompensation([FromBody] Compensation compensation)
+        //public IActionResult CreateCompensation([FromBody] Compensation compensation)
+        public IActionResult CreateCompensation([FromBody] SaveCompensationResource compensationResource)
+        {
+            //_logger.LogDebug($"Received compensation create request for '{compensation.Employee.FirstName} {compensation.Employee.LastName}'");
+
+            
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.GetErrorMessages());
+            }
+
+            var compensation = _mapper.Map<SaveCompensationResource, Compensation>(compensationResource);
+            var result = _compensationService.Create(compensation);
+
+            if (!result.Success)
+            {
+                return BadRequest(result.Message);
+            }
+
+            var compResource = _mapper.Map<Compensation, CompensationResource>(result.Compensation);
+            return Ok(compResource);
+            //return CreatedAtRoute("getEmployeeById", new { id = compensation.EmployeeId }, compensation);
+            //return CreatedAtRoute("getEmployeeById", new { id = compensation.EmployeeId }, compensation);
+        }
+
+        [HttpGet("{id}", Name = "getCompensationById")]
+        public IActionResult GetCompensationById(String id)
+        {
+            _logger.LogDebug($"Received compensation get request for '{id}'");
+
+            var compensation = _compensationService.GetById(id);
+
+            if (compensation == null)
+                return NotFound();
+
+            return Ok(compensation);
+        }
+
+    }
+}
---
+++ ReportingStructureController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using challenge.Services;
+using challenge.resources;
+using Microsoft.AspNetCore.Mvc;
+using challenge.Models;
+using Microsoft.Extensions.Logging;
+
+namespace challenge.Controllers
+{
+    [Route("api/reportingstructure")]
+    public class ReportingStructureController : Controller
+    {
+        private readonly ILogger _logger;
+        private readonly IMapper _mapper;
+        private readonly IReportingStructureService _reportingStructureService;
+
+        public ReportingStructureController (ILogger<ReportingStructureController> logger, IReportingStructureService reportingStructureService, IMapper mapper)
+        {
+            _logger = logger;
+            _reportingStructureService = reportingStructureService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id}", Name = "getReportingStructureById")]
+        public IActionResult GetReportingStructureById(String id)
+        {
+            _logger.LogDebug($"Received reporting structure get request for '{id}'");
+
+            var reportingStructure = _reportingStructureService.GetById(id);
+            var resource = _mapper.Map<ReportingStructure, ReportingStructureResource>(reportingStructure);
+
+            if (reportingStructure == null)
+                return NotFound();
+
+            return Ok(resource);
+            //return Ok(reportingStructure);
+        }
+
+
+        /*
+        [HttpGet]
+        public async Task<IEnumerable<ReportingStructureResource>> GetAllAsync()
+        {
+            var reporintgStructure = await _reportingStructureService.ListAsync();
+            var resources = _mapper.Map<IEnumerable<ReportingStructure>,  IEnumerable<ReportingStructureResource>>(reporintgStructure);
+            return resources;
+        }
+        */
+    }
+}
